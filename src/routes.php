2452a19252f33a5cb3d8e68569bfbc08513957e6
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-12-08 13:15:50 +0100

    Bejelentkezés elkezdve

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use Kilianmarcell\Login\Token;
 use Kilianmarcell\Login\User;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -10,6 +11,7 @@ return function(App $app) {
         $response->getBody()->write("Hello world!");
         return $response;
     });
+
     $app->post('/register', function(Request $request, Response $response, $args) {
         $userData = json_decode($request->getBody(), true);
         $user = new User();
@@ -17,6 +19,22 @@ return function(App $app) {
         $user->password = password_hash($userData['password'], PASSWORD_DEFAULT);
         $user->save();
         $response->getBody()->write($user->toJson());
+        return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus(201);
+    });
+
+    $app->post('/login', function(Request $request, Response $response, $args) {
+        $loginData = json_decode($request->getBody(), true);
+        $email = $loginData['email'];
+        $password = $loginData['password'];
+        $user = User::where('email', $email)->firstOrFail();
+        if (password_verify($password, $user->password)) {
+            throw new Exception('Hibás email vagy jelszó!');
+        }
+        $token = new Token();
+        $token->user_id = $user->id;
+        $token->token = bin2hex(random_bytes(64)); //16 os számrendszerben eltárol egy random 64 bájtos random szöveget
         return $response
             ->withHeader('Content-Type', 'application/json')
             ->withStatus(201);
